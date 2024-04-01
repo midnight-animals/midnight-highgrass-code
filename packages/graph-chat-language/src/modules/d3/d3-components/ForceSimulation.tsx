3cb0a7918590a6c0ea4308eb13609aaa9d892d20
hdn <hieudn5293@yahoo.de>
2024-04-01 16:32:58 +0200

    save

--- packages/graph-chat-language/src/modules/d3/d3-components/ForceSimulation.tsx
+++ packages/graph-chat-language/src/modules/d3/d3-components/ForceSimulation.tsx
@@ -1,80 +1,133 @@
 import * as d3 from 'd3';
 import { SimulationNodeDatum } from 'd3';
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import './ForceSimulation.scss';
 
 const width = 600;
 const height = 400;
-const numNodes = 100;
-const FORCE_STRENGTH_INITIAL = 1;
-const FORCE_STRENGTH_BOUNDARY = 10;
-const FORCE_STRENGTH_STEP = 0.5;
+const numNodes = 8;
+const IS_FORCE_ACTIVE = true;
+const IS_X_COLLIDE = false;
+const IS_Y_COLLIDE = false;
+const FORCE_STRENGTH_INITIAL = 10;
+const FORCE_STRENGTH_BOUNDARY = 100;
+const FORCE_STRENGTH_STEP = 10;
 const FORCE_COLLIDE_INITIAL = 0;
 const FORCE_COLLIDE_BOUNDARY = 50;
-const FORCE_COLLIDE_STEP = 1;
+const FORCE_COLLIDE_STEP = 10;
+const LINK_DISTANCE_INITIAL = 30;
+const LINK_DISTANCE_BOUNDARY = 50;
+const LINK_DISTANCE_STEP = 10;
 const NODE_RADIUS_INITIAL = 0;
 const NODE_RADIUS_BOUNDARY = 20;
 const NODE_RADIUS_STEP = 1;
+const NODE_RADIUS_MAX = 10;
 
+const xCenter = [100, 300, 500];
 const ticksPerRender = 3;
 // const ALPHA_THRESHOLD = 0.0000000001;
-// const ALPHA_THRESHOLD = 0.000001;
-const ALPHA_THRESHOLD = 0.01;
+const ALPHA_THRESHOLD = 0.000001;
+// const ALPHA_THRESHOLD = 0.01;
+// const ALPHA_THRESHOLD = 0.99;
+const xScale = d3.scaleLinear().domain([0, 1]).range([0, 600]);
+
+const links = [
+  { source: 0, target: 1 },
+  { source: 0, target: 2 },
+  { source: 0, target: 3 },
+  { source: 1, target: 6 },
+  { source: 3, target: 4 },
+  { source: 3, target: 7 },
+  { source: 4, target: 5 },
+  { source: 4, target: 7 },
+];
 
 interface D3Node extends SimulationNodeDatum {
   radius: number;
+  category: number;
+  value: number;
+  name: string;
   x: number;
   y: number;
 }
 
-let nodes = d3.range(numNodes).map(function () {
-  return { radius: Math.random() * 25 };
+const nodes = d3.range(numNodes).map(function (index) {
+  return {
+    name: index.toString(),
+    radius: Math.random() * NODE_RADIUS_MAX,
+    value: Math.random(),
+    category: Math.floor(Math.random() * 3),
+  };
 }) as SimulationNodeDatum[];
+/* prettier-ignore */ console.log('>>>> _ >>>> ~ nodes:', nodes);
 
 function resetNodes() {
-  nodes = d3.range(numNodes).map(function () {
-    return { radius: Math.random() * 25 };
-  }) as SimulationNodeDatum[];
+  nodes.forEach((node: D3Node) => {
+    node.radius = Math.random() * NODE_RADIUS_MAX;
+    node.category = Math.floor(Math.random() * 3);
+  });
+  // nodes = d3.range(numNodes).map(function () {
+  //   return {
+  //     radius: Math.random() * NODE_RADIUS_MAX,
+  //     category: Math.floor(Math.random() * 3),
+  //   };
+  // }) as SimulationNodeDatum[];
 }
 
 export const D3ForceSimulation: React.FC = (): JSX.Element => {
-  const ref = useRef<SVGSVGElement>(null);
-  const [isForceCollideActive, setIsForceCollideActive] = useState(false);
+  const svgRef = useRef<SVGSVGElement>(null);
+  const [isForceCollideActive, setIsForceCollideActive] =
+    useState(IS_FORCE_ACTIVE);
+  const [isXCollide, setIsXCollide] = useState(IS_X_COLLIDE);
+  const [isYCollide, setIsYCollide] = useState(IS_Y_COLLIDE);
   const [centerForce, setCenterForce] = useState(width / 2); // New state for center force
   const [forceStrength, setForceStrength] = useState(FORCE_STRENGTH_INITIAL);
   const [forceCollide, setForceCollide] = useState(FORCE_COLLIDE_INITIAL);
+  const [linkDistance, setLinkDistance] = useState(LINK_DISTANCE_INITIAL);
   const [nodeRadius, setNodeRadius] = useState(NODE_RADIUS_INITIAL);
 
-  const ticked = () => {
-    // console.log('--');
-    const svgElement = d3.select(ref.current);
-    svgElement
-      .selectAll('circle')
-      .style('color', 'cadetblue')
-      .data(nodes)
-      .join('circle')
-      // .attr('r', (d) => 5)
-      .attr('r', (d) => (d as D3Node).radius + nodeRadius)
-      .attr('cx', (d) => (d as D3Node).x)
-      .attr('cy', (d) => (d as D3Node).y);
-  };
-
   const simulation = useMemo(() => {
-    return d3
-      .forceSimulation(nodes)
-      .force('center', d3.forceCenter(centerForce, height / 2)) // Use centerForce state
-      .force(
-        'collision',
-        d3.forceCollide().radius(function (d) {
-          // return 5;
-          return (d as D3Node).radius + nodeRadius;
-        }),
-      );
+    return (
+      d3
+        .forceSimulation(nodes)
+        .force('center', d3.forceCenter(centerForce, height / 2)) // Use centerForce state
+        .force(
+          'collision',
+          d3.forceCollide().radius(function (d) {
+            // return 5;
+            return (d as D3Node).radius + nodeRadius;
+          }),
+        )
+        .force(
+          'x',
+          d3.forceX().x(function (d) {
+            return xCenter[(d as D3Node).category];
+          }),
+        )
+        // .force(
+        //   'x',
+        //   d3.forceX().x(function (d) {
+        //     return xScale((d as D3Node).value);
+        //   }),
+        // )
+        // .force(
+        //   'y',
+        //   d3.forceY().y(function (d) {
+        //     return 0;
+        //   }),
+        // )
+        .force('link', d3.forceLink().links(links).distance(linkDistance))
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [
     nodes,
     isForceCollideActive,
+    isXCollide,
+    isYCollide,
     centerForce,
     forceStrength,
     forceCollide,
+    linkDistance,
     nodeRadius,
   ]);
   const simulationRef = useRef(simulation);
@@ -102,7 +155,8 @@ export const D3ForceSimulation: React.FC = (): JSX.Element => {
         simulationRef.current.tick();
       }
 
-      ticked();
+      updateCircles();
+      updateLinks();
 
       if (simulationRef.current.alpha() > ALPHA_THRESHOLD) {
         requestAnimationFrame(render);
@@ -116,6 +170,12 @@ export const D3ForceSimulation: React.FC = (): JSX.Element => {
     if (isForceCollideActive) {
       simulationRef.current.force('charge', null);
       simulationRef.current.force('collision', null);
+      if (isXCollide) {
+        simulationRef.current.force('x', null);
+      }
+      if (isYCollide) {
+        simulationRef.current.force('y', null);
+      }
     } else {
       simulationRef.current.force(
         'charge',
@@ -132,11 +192,15 @@ export const D3ForceSimulation: React.FC = (): JSX.Element => {
         );
     }
     startTicking();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [
     centerForce,
     forceCollide,
     forceStrength,
     isForceCollideActive,
+    isXCollide,
+    isYCollide,
+    linkDistance,
     nodeRadius,
   ]);
 
@@ -150,6 +214,46 @@ export const D3ForceSimulation: React.FC = (): JSX.Element => {
     handleForceChange,
   ]);
 
+  const updateCircles = () => {
+    // console.log('--');
+    const svgElement = d3.select(svgRef.current);
+    svgElement
+      // .style('color', 'cadetblue')
+      .selectAll('text')
+      .data(nodes)
+      .join('text')
+      .text(function (d) {
+        return (d as D3Node).name;
+      })
+      // .attr('r', (d) => 5)
+      .attr('r', (d) => (d as D3Node).radius + nodeRadius)
+      .attr('x', (d) => (d as D3Node).x)
+      .attr('y', (d) => (d as D3Node).y);
+    // .attr('cx', (d) => (d as D3Node).x)
+    // .attr('cy', (d) => (d as D3Node).y);
+  };
+
+  function updateLinks() {
+    const svgElement = d3.select(svgRef.current);
+    svgElement
+      .selectAll('line')
+      .style('stroke', '#ccc')
+      .data(links)
+      .join('line')
+      .attr('x1', function (d) {
+        return d.source.x;
+      })
+      .attr('y1', function (d) {
+        return d.source.y;
+      })
+      .attr('x2', function (d) {
+        return d.target.x;
+      })
+      .attr('y2', function (d) {
+        return d.target.y;
+      });
+  }
+
   return (
     <>
       <section>
@@ -157,18 +261,42 @@ export const D3ForceSimulation: React.FC = (): JSX.Element => {
           width={width}
           height={height}
           style={{ border: '1px solid red' }}
-          ref={ref}
+          ref={svgRef}
         ></svg>
       </section>
+
       <section>
-        isForceCollideActive
-        <input
-          type="checkbox"
-          checked={isForceCollideActive}
-          onChange={() => {
-            setIsForceCollideActive(!isForceCollideActive);
-          }}
-        />
+        <div>
+          isForceCollideActive
+          <input
+            type="checkbox"
+            checked={isForceCollideActive}
+            onChange={() => {
+              setIsForceCollideActive(!isForceCollideActive);
+            }}
+          />
+        </div>
+        <div>
+          x collide
+          <input
+            type="checkbox"
+            checked={isXCollide}
+            onChange={() => {
+              setIsXCollide(!isXCollide);
+            }}
+          />
+        </div>
+        <div>
+          y collide
+          <input
+            type="checkbox"
+            checked={isYCollide}
+            onChange={() => {
+              setIsYCollide(!isYCollide);
+            }}
+          />
+        </div>
+
         <div>
           Force center
           <input
@@ -199,6 +327,17 @@ export const D3ForceSimulation: React.FC = (): JSX.Element => {
             max={FORCE_COLLIDE_BOUNDARY}
             value={forceCollide}
             onChange={(event) => setForceCollide(Number(event.target.value))}
+          />
+        </div>
+        <div>
+          Link Distance
+          <input
+            type="number"
+            step={LINK_DISTANCE_STEP}
+            min={-LINK_DISTANCE_BOUNDARY}
+            max={LINK_DISTANCE_BOUNDARY}
+            value={linkDistance}
+            onChange={(event) => setLinkDistance(Number(event.target.value))}
           />
         </div>
         <div>
